4080c0d3ac373ac6d1723b69dea2e1b2964969ef
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-03-01 00:42:59 +0100

    Add presentation name, desc and image
    Make mode only light

---
+++ src/components/cv-base.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import React from 'react'
+
+const CVComponent = () => {
+  return (
+    <div className="text-center flex gap-4">
+      <div>
+        <Image src="/profilepic.png" alt="Vercel Logo" className="dark" width={200} height={24} priority />
+      </div>
+      <div>
+        <h1 className="text-5xl font-bold py-1">Atanas Yonkov</h1>
+        <p className="text-3xl py-1">Junior Software Engineer</p>
+      </div>
+    </div>
+  )
+}
+
+export default CVComponent
