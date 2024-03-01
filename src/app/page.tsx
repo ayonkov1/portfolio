38c7b4d6f97e8601bf4b9c60d4a009d2b4172d91
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-03-01 14:39:19 +0100

    update page.tsx and cv-base.tsx
    Refactor UI layout and integrate Summary section
    
    Reorganized the UI for improved readability and visual appeal. Introduced a new Summary section component to provide a concise overview of professional qualifications and objectives, enhancing user engagement. Adjustments to page structure and styling ensure a more cohesive and polished appearance. This change aims to better highlight key information and improve the overall user experience.
    
    - Simplified layout in `cv-base.tsx` for clarity and focus.
    - Added Summary component for a succinct professional summary.
    - Adjusted styling and positioning in `page.tsx` for better visual coherence and accessibility.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import CVComponent from '@/components/custom/cv-base'
+import Summary from '@/components/custom/sections/summary'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="h-screen w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex justify-center pt-8">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] absolute flex justify-center p-10">
+        <div className="fixed bg-cover pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_80%,black)]"></div>
         <CVComponent />
       </div>
     </main>
--- src/components/custom/cv-base.tsx
+++ src/components/custom/cv-base.tsx
@@ -2,29 +2,45 @@ import Image from 'next/image'
 import React from 'react'
 import { ModeToggle } from '../ui/light-dark-toggle'
 import { SocialButton } from './social-buttons'
+import Summary from './sections/summary'
 
 const CVComponent = () => {
   return (
-    <div className="text-center flex flex-col lg:flex-row items-center lg:items-start gap-10">
-      <div>
-        <Image
-          src="/profilepic.png"
-          alt="Atanas Yonkov Profile Picture"
-          className="rounded-lg shadow-xl drop-shadow-xl shadow-neutral-700"
-          width={200}
-          height={24}
-          priority
-        />
+    <div className="py-2 md:px-10 lg:px-20 xl:px-32 2xl:px-72">
+      <div className="text-center lg:text-left flex flex-col lg:flex-row items-center lg:items-start gap-10 justify-start">
+        <div>
+          <Image
+            src="/profilepic.png"
+            alt="Atanas Yonkov Profile Picture"
+            className="rounded-lg shadow-xl drop-shadow-xl shadow-neutral-700"
+            width={200}
+            height={24}
+            priority
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-2xl lg:text-6xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-tr from-neutral-200 to-neutral-700">
+            Atanas Yonkov
+          </h1>
+          <p className="text-lg lg:text-3xl py-1">Junior Software Developer</p>
+          <div className="flex gap-4 align-middle items-center justify-center lg:justify-start">
+            <ModeToggle />
+            <SocialButton variant="Github" />
+            <SocialButton variant="Linkedin" />
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col gap-4">
-        <h1 className="text-2xl lg:text-6xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-tr from-neutral-200 to-neutral-700">
-          Atanas Yonkov
-        </h1>
-        <p className="text-lg lg:text-3xl py-1">Junior Software Developer</p>
-        <div className="flex gap-4 align-middle items-center justify-center">
-          <ModeToggle />
-          <SocialButton variant="Github" />
-          <SocialButton variant="Linkedin" />
+      <div className="flex flex-col gap-3 py-3 lg:gap-10 lg:py-10">
+        <div>
+          <Summary />
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <Summary />
+          <Summary />
+        </div>
+        <div className="flex flex-col lg:flex-row">
+          <Summary />
+          <Summary />
         </div>
       </div>
     </div>
