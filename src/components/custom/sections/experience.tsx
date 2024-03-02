1cc603f873fe0a6eed94771ee4d883d90d478065
Atanas Yonkov <atanasyonkov1@gmail.com>
2024-03-02 17:57:37 +0100

    Update experience and summary sections, add data file

--- src/components/custom/sections/experience.tsx
+++ src/components/custom/sections/experience.tsx
@@ -1,45 +1,6 @@
 import { Title } from '@/components/core/title'
 import React from 'react'
-
-interface Role {
-  title?: string
-  description: string
-}
-
-interface Experience {
-  title: string
-  company: string
-  roles: Role[]
-  duration: string
-}
-
-const experiences: Experience[] = [
-  {
-    title: 'Master Thesis Researcher',
-    company: 'CWI',
-    roles: [
-      {
-        description: 'Developed RESTful APIs and integrated with databases using Node.js and Express.',
-      },
-    ],
-    duration: 'April 2022 – Aug. 2023',
-  },
-  {
-    title: 'Junior Software Engineer',
-    company: 'Merkle',
-    roles: [
-      {
-        title: 'Frontend Developer',
-        description: 'Worked on building responsive user interfaces using React and styled components.',
-      },
-      {
-        title: 'Backend Developer',
-        description: 'Developed RESTful APIs and integrated with databases using Node.js and Express.',
-      },
-    ],
-    duration: 'April 2022 – Aug. 2023',
-  },
-]
+import { experiences } from '@/components/data/experiences'
 
 function ExperienceSection() {
   return (
@@ -47,15 +8,17 @@ function ExperienceSection() {
       <Title text="Experience" />
 
       {experiences.map((exp, index) => (
-        <div key={index} className="text-sm lg:text-lg py-1 pb-10">
-          <p className="text-lg lg:text-2xl font-semibold pt-1">{`${exp.title} at ${exp.company}`}</p>
+        <div key={index} className="text-sm lg:text-lg py-1 pb-6">
+          <p className="text-md lg:text-2xl font-semibold pt-1 text-green-900 dark:text-purple-400">{`${exp.title} at ${exp.company}`}</p>
           <p className="italic">{exp.duration}</p>
-          <div className="pt-4">
+          <div className="">
             {exp.roles.map((role, roleIndex) => (
               <div key={roleIndex}>
-                <p className="font-semibold">{role.title}</p>
-                <ul className="list-disc pl-4">
-                  <li>{role.description}</li>
+                <p className="font-semibold pt-4">{role.title}</p>
+                <ul className="list-disc pl-4 text-base">
+                  {role.description.map((desc, index) => (
+                    <li key={index}>{desc}</li>
+                  ))}
                 </ul>
               </div>
             ))}
--- src/components/custom/sections/summary.tsx
+++ src/components/custom/sections/summary.tsx
@@ -6,7 +6,7 @@ const Summary = () => {
     <div>
       <Title text="Summary" />
 
-      <p className="text-sm lg:text-lg py-1 ">
+      <p className="text-sm lg:text-lg py-1 bg-clip-text z-20 ">
         Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
         ex esse exercitation amet. Nisi animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip
         amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
